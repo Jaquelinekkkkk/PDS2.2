7c1d49c987fc7ce1c9a3f25372ebc6ccefc0296a
jaquelinekkkkk <jaqueline.c2004@aluno.ifsc.edu.br>
2025-07-10 01:32:36 -0300

    cadastros

--- PDS2-I6-25-1-main/PDS2-I6-25-1-main/LojaMVC/lojabd_usuarios.sql
+++ PDS2-I6-25-1-main/PDS2-I6-25-1-main/LojaMVC/lojabd_usuarios.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `lojabd` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `lojabd`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lojabd
@@ -26,12 +24,14 @@ DROP TABLE IF EXISTS `cliente`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `endereco` varchar(225) DEFAULT NULL,
   `nome` varchar(100) NOT NULL,
   `telefone` varchar(20) DEFAULT NULL,
-  `endereco` varchar(225) DEFAULT NULL,
   `data_nascimento` date DEFAULT NULL,
+  `login` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'rua','ela','(47)98878-6532','2004-04-13','ela','1234');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +87,7 @@ CREATE TABLE `produto` (
   `valor` decimal(10,2) NOT NULL,
   `quantidade_estoque` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,6 +96,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'pao',10.00,20),(2,'bolo de chocolate',25.00,20),(3,'capuccino',12.00,200),(4,'cafe expresso',3.00,200),(5,'cafe expresso',3.00,200),(6,'queijo',15.00,70),(7,'boo',3.00,34);
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +118,7 @@ CREATE TABLE `usuarios` (
   `email` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login_UNIQUE` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +127,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Fulano','234234','fulano','1234','admin','2000-12-14',''),(2,'Beltrano','234234243','beltrano','1234','user','2006-06-07','');
+INSERT INTO `usuarios` VALUES (1,'Fulano','234234','fulano','1234','admin','2000-12-14',''),(2,'Beltrano','234234243','beltrano','1234','user','2006-06-07',''),(3,'q','12','q','12','user','2004-07-08','1'),(4,'Jaque','47988808621','47988808621','123','user','2004-04-13','cliente@email.com'),(6,'que','123','quee','123','admin','2025-07-15','1'),(7,'qqq','123','qqq','123','admin','2002-07-11','qqq'),(8,'cliente','47988808621','cliente','123','user','2003-07-09','cliente'),(12,'cllll','47988878621','47988878621','123','user','2004-04-13','cliente@email.com'),(13,'www','(47)98880-5423','www','123','admin','2015-07-09','www'),(14,'aaa','4799904-8789','aaa','1234','user','2016-07-01','aaa');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,9 +143,11 @@ CREATE TABLE `venda` (
   `data_compra` date NOT NULL,
   `valor_total` decimal(10,2) DEFAULT NULL,
   `cliente_id` int DEFAULT NULL,
+  `id_cliente` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cliente_id` (`cliente_id`),
-  CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
+  KEY `fk_venda_cliente` (`id_cliente`),
+  CONSTRAINT `fk_venda_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -165,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-05 11:07:34
+-- Dump completed on 2025-07-10  1:32:15
